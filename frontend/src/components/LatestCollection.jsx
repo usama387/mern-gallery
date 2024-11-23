a2013fc407166e5878f2e94d89ce067c8c235ac6
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-23 12:21:26 +0500

    LatestCollection component mounted on Home Page with child components in frontend

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+
+  //   this state holds 10 products data from products whenever it is mounted
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products]);
+
+  return (
+    <div className="my-10">
+      {/* Texts */}
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 text-base sm:text-xs m-auto md:text-xl text-gray-600">
+          Discover our latest products
+        </p>
+      </div>
+
+      {/* Mapping and sending product details to child component as props to render it*/}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <div key={index}>
+            <ProductItem
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
+
+const ProductItem = ({ id, name, image, price }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          src={image[0]}
+          alt=""
+          className="hover:scale-110 transition ease-in-out"
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-base font-medium">
+        {price} {currency}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductItem;
