c07ec277269b69b91d150f553ab27cf461fb60a4
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-04 21:37:28 +0500

    Received  setToken state as props from App.jsx to to reset it to empty string and logout

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <p
@@ -9,7 +9,12 @@ const Navbar = () => {
       >
         GALLERY
       </p>
-      <button className="bg-teal-500 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-xs">Logout</button>
+      <button
+        className="bg-teal-500 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-xs"
+        onClick={() => setToken("")}
+      >
+        Logout
+      </button>
     </div>
   );
 };
