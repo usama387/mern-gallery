047c08eb39240fa78df96c5e419afb09d1d2c81c
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-04 18:52:54 +0500

    Added Routes configuration

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddProduct from "./pages/AddProduct";
+import ListProducts from "./pages/ListProducts";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
@@ -10,6 +14,13 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<AddProduct />} />
+              <Route path="/list" element={<ListProducts />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
         </div>
       </>
     </div>
