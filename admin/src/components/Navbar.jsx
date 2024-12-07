5748671ca4804c74fe624bfa1a8f9384ed334351
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-07 14:37:31 +0500

    Added modifications

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,14 +3,22 @@ import React from "react";
 const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <p
-        className="w-3 text-[#f472b6] text-3xl sm:text-xl font-semibold cursor-pointer"
-        onClick={() => navigate("/")}
-      >
-        GALLERY
-      </p>
+      <div className="flex items-center">
+        {/* GALLERY Text */}
+        <p
+          className="text-teal-600 text-4xl sm:text-xl font-semibold cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          GALLERY
+        </p>
+        {/* Admin Label */}
+        <span className="ml-2 px-2 py-1 text-xs sm:text-sm bg-yellow-400 text-black rounded-lg font-medium">
+          Admin
+        </span>
+      </div>
+
       <button
-        className="bg-teal-500 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-xs"
+        className="bg-yellow-400 text-black px-4 py-4 sm:px-7 text-2xl sm:text-xs font-bold transition-transform duration-300 hover:scale-105"
         onClick={() => setToken("")}
       >
         Logout
