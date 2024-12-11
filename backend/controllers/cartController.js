29bd610ed9b2a56d3a6c31537881e9b0a0bc5509
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-11 11:19:30 +0500

    Added cartController file in controllers folder

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+const addProductToCart = async (req, res) => {};
+const updateProductCart = async (req, res) => {};
+const getUserCartDetail = async (req, res) => {};
+
+export { addProductToCart, updateProductCart, getUserCartDetail };
