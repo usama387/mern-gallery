85f03b92caf2bb31cc732754dd1dbc66f6c9e8ba
usama387 <128306049+usama387@users.noreply.github.com>
2024-12-13 13:52:22 +0500

    Added a new orderRoute for oderController apis in routes folder

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  allOrdersForAdminPanel,
+  orderDetailsForUser,
+  orderPlaceWithCod,
+  orderPlaceWithStripe,
+  orderStatusUpdate,
+} from "../controllers/orderController.js";
+import verifyAdmin from "../middleware/verifyadmin.js";
+import verifyUser from "../middleware/verifyUser.js";
+
+const orderRouter = express.Router();
+
+// api routes for admin
+orderRouter.post("/list", verifyAdmin, allOrdersForAdminPanel);
+orderRouter.post("/status", verifyAdmin, orderStatusUpdate);
+
+// api routes for payments
+orderRouter.post("/place", verifyUser, orderPlaceWithCod);
+orderRouter.post("/stripe", verifyUser, orderPlaceWithStripe);
+
+// api route for user
+orderRouter.post("/userOrders", verifyUser, orderDetailsForUser);
+
+export default orderRouter;
